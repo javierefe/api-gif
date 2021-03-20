09f72eadafcb83d5fb18887a91dbcb7d5e69f899
javierefe <jcfloresq@gmail.com>
2021-03-20 10:35:32 -0500

    nuevo theme

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({id, title, url}) => {
     return (
         <div className="card animate__animated animate__fadeIn">
           <img src={url} alt={title}/>
-          <p>{title}</p>
+          <p><strong>{title}</strong></p>
         </div>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -4,7 +4,7 @@ export const getGifs = async(category) => {
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=2hvXk2v8sfRYdbMhUWuhF0y0aIr3jQ2O`
     const resp = await fetch(url);
     const {data} = await resp.json();
-
+    
     const gifs = data.map( img => {
 
         return {
